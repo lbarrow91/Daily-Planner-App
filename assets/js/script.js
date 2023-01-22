78f9e875c487c777a128267c80f0b172bcaf41c2
lbarrow91 <laurabarrow1991@gmail.com>
2023-01-22 16:20:29 +0000

    Colour code each block based on the current time

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,18 @@
+// Show the current date at the top
 $('#currentDay').html(moment().format('dddd, MMMM Do'))
 
+// Colour code each block based on the current time
+var timeblocks = $('.timeblock');
+var hourEl = $('data-hour')
+
+var currentHour = (new Date()).getHours();
+    $('.timeblock').each(function(){
+        var hour = parseInt($(this).attr('data-hour')); 
+        if(hour < currentHour){
+            $(this).css('background-color','Grey');
+        }else if(hour === currentHour){
+            $(this).css('background-color','Red');
+        }else {
+            $(this).css('background-color', 'Green');
+        }
+    });
